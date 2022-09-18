69485538d5321817ee581b8f750582dedd61e6a8
Kirsi Jokela <kirsijo@gmail.com>
2022-09-18 18:43:55 +0300

    first test passing

--- Jokela_Kirsi_book/__tests__/bookStorage.test.js
+++ Jokela_Kirsi_book/__tests__/bookStorage.test.js
@@ -4,5 +4,7 @@ const books = require('../datastorage.json');
 const BookStorage = require('../BookStorage');
 
 describe('Testing constructor', () => {
-    
+    test('Test that the missing data throws an exception', () => {
+        expect(()=> new BookStorage().toThrow('phone data missing'))
+    })
 })
